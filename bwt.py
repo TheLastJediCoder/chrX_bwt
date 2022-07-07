bb85567e30454dadb6955b0b18eeb5a2f3ddb40c
Harsh Parecha <harshparecha@gmail.com>
2022-07-07 13:18:40 +0530

    bwt map read updated

--- bwt.py
+++ bwt.py
@@ -1,115 +1,95 @@
-import sys
-# first_column_index = open('chrX_map.txt', 'r').readlines()
-# first_column_file = open('chrX.fa', 'r').read().replace('\n', '')
-# first_column_file_len = len(first_column_file)
-# # print(last_col_file)
-# print(first_column_file[first_column_file_len-1])
-# rank = {'A': [],
-#         'C': [],
-#         'G': [],
-#         'T': []}
+# bit_array = {'A': [], 'C': [], 'G': [], 'T': []}
+bit_array = []
+rank = {'A': [0], 'C': [0], 'G': [0], 'T': [0]}
+total = {'A': 0, 'C': 0, 'G': 0, 'T': 0}
+delta = [15]
 
-# count = 1
-# for index in first_column_index:
-#     index_ = int(index.strip())+5
-#     if index_ >= first_column_file_len:
-#         index_ -= 1
-#     character = first_column_file[index_]
-#     # print(character)
-#     if count % 100 == 0:
-#         if character in rank:
-#             rank[character].append(count)
-#     count += 1
+last_col_file = open('data/chrX_last_col.txt', 'r')
 
-delta = 15
-
-last_column_file = open('data/chrX_last_col.txt', 'r').read()
-
-occurance_of_characters = {base:[False for i in range(len(last_column_file))] for base in ['A', 'T', 'C', 'G']}
-
-length_of_last_column_file = len(last_column_file)
-
-# occurance_A = [False for i in range(len(length_of_last_column_file))]
-# occurance_C = [False for i in range(len(length_of_last_column_file))]
-# occurance_G = [False for i in range(len(length_of_last_column_file))]
-# occurance_T = [False for i in range(len(length_of_last_column_file))]
-
-rank_of_characters = {base:[] for base in ['A','T','C','G']}
-
-# rank_A = []
-# rank_C = []
-# rank_G = []
-# rank_T = []
-
-index = 0
-
-count_of_characters = {base:0 for base in ['A', 'T', 'C', 'G']}
-
-# count_A = 0
-# count_C = 0
-# count_G = 0
-# count_T = 0
-
-while(index < length_of_last_column_file):
-    character = last_column_file[index]
-    if(not character):
-        break
-    if(character!='$' and character!='\n'):
-        occurance_of_characters[character][index] = True
-        count_of_characters[character]+=1
-        # if character == 'A':
-        #     occurance_A[index] = True
-        # elif character == 'C':
-        #     occurance_C[index] = True
-        # elif character == 'G':
-        #     occurance_G[index] = True
-        # elif character == 'T':
-        #     occurance_T[index] = True
-
-        if(index > 0 and index % delta == 0):
-            for b in ['A', 'T', 'C', 'G']:
-                rank_of_characters[b].append(count_of_characters[b])
-            # rank_A.append(count_A)
-            # rank_C.append(count_C)
-            # rank_G.append(count_G)
-            # rank_T.append(count_T)
+i = 0
+while(True):
+    char = last_col_file.read(1)
+    if char == '\n':
+        char = last_col_file.read(1)
+    if i == delta[0]:
+        rank['A'].append(total['A'])
+        rank['C'].append(total['C'])
+        rank['G'].append(total['G'])
+        rank['T'].append(total['T'])
+        i = 0
     
-    index += 1
+    if char in total:
+        total[char] += 1
 
-def rank(char, index, delta):
-    print(f'Rank of chr {char} at index {index}')
-    if index % delta == 0:
-        return rank_of_characters[char][int(index/delta)]
-    else:
-        if index % delta > delta / 2:
-            lower_limit = index
-            upper_limit = index + (delta - (index % delta))
-            rank_index = int(upper_limit/delta)
-            return rank_of_characters[char][rank_index-1] - occurance_of_characters[char][lower_limit+1:upper_limit+1].count(True)
-        else:
-            lower_limit = index - (index % delta)
-            upper_limit = index
-            rank_index = int(lower_limit/delta)
-            if rank_index == 0:
-                return occurance_of_characters[char][lower_limit:upper_limit+1].count(True)
-            else:    
-                return rank_of_characters[char][rank_index-1] + occurance_of_characters[char][lower_limit:upper_limit+1].count(True)
+    bit_array.append(char)
+    # bit_array['A'].append(True if char == 'A' else False)
+    # bit_array['C'].append(True if char == 'C' else False)
+    # bit_array['G'].append(True if char == 'G' else False)
+    # bit_array['T'].append(True if char == 'T' else False)
 
-print(rank('C', 500, delta))
-print(rank('C', 700, delta))
-print(rank('C', 900, delta))
-print(rank('C', 1600, delta))
-print(rank('C', 5000, delta))
-print(rank('C', 7000, delta))
-print(rank('C', 9000, delta))
-print(rank('C', 16000, delta))
+    if not char:
+        break
+    i += 1
 
-# print(sys.getsizeof(occurance_of_characters['A']) +
-#         sys.getsizeof(occurance_of_characters['C'])+
-#         sys.getsizeof(occurance_of_characters['G'])+
-#         sys.getsizeof(occurance_of_characters['T']))
+def rank_last_col(char, index):
+    # if index > len(bit_array['A']) or index < 0:
+    if index > len(bit_array) or index < 0:
+        print('Index out of bound')
+        return -1
+    delta_index = int(index / delta[0])
+    delta_index_mult = int(delta_index * delta[0])
+    res = 0
+    res = rank[char][delta_index]
+    for j in range(delta_index_mult, index):
+        # if bit_array[char][j]:
+        #     res += 1
+        if bit_array[j] == char:
+            res += 1
+    return res
 
-# print(sys.getsizeof(rank_of_characters['A']) + 
-# sys.getsizeof(rank_of_characters['C']) +
-# sys.getsizeof(rank_of_characters['G']) +
-# sys.getsizeof(rank_of_characters['T']))
+band = {}
+band['A'] = 0
+band['C'] = total['A']
+band['G'] = total['A'] + total['C']
+band['T'] = total['A'] + total['C'] + total['G']
+
+def select_first_col(i, char):
+    return band[char] + i - 1
+
+def map_read(reads):
+    char = reads[-1]
+    band_start = select_first_col(1, char)
+
+    count = total[char]   
+    band_end = select_first_col(count, char)
+
+    for i in range(len(reads)-2, -1, -1):
+        # print(band_start, band_end)
+        top = rank_last_col(reads[i], band_start)
+        bottom = rank_last_col(reads[i], band_end)
+        # print(top, bottom, reads[i])
+        bottom_inclusive = bottom
+        # if (bit_array['A'][band_end] and reads[i] == 'A') or (bit_array['C'][band_end] and reads[i] == 'C') or (bit_array['G'][band_end] and reads[i] == 'G') or (bit_array['T'][band_end] and reads[i] == 'T'):
+        #     bottom_inclusive += 1
+        if (bit_array[band_end] == 'A' and reads[i] == 'A') or (bit_array[band_end] == 'C' and reads[i] == 'C') or (bit_array[band_end] == 'G' and reads[i] == 'G') or (bit_array[band_end] == 'T' and reads[i] == 'T'):
+            bottom_inclusive += 1
+        if bottom_inclusive == top:
+            return -1, -1
+        band_start = select_first_col(top+1, reads[i])
+        band_end = select_first_col(bottom_inclusive, reads[i])
+    
+    return band_start, band_end
+
+read = 'TGGCCAAATCTTAGCTATTTGAGGAATGTAGGGAGAAAAGCCACCTTCTCTCTCTATGTCTGAAGGTTCCCATGGCTGTCTCTTTGCCCAAGGGGCAAACT'
+positions = map_read(read)
+
+get_index_to_reference_sequence = open('data/chrX_map.txt', 'r').readlines()
+reference_file = open('data/chrX.fa', 'r').read().replace('\n', '')
+index_to_reference_sequence = int(get_index_to_reference_sequence[positions[0]].strip())
+reference_read = reference_file[index_to_reference_sequence:index_to_reference_sequence+len(read)]
+
+print(f'Positions      : {positions}')
+print(f'Index in rfile : {index_to_reference_sequence}')
+print(f'Actual Read    : {read}')
+print(f'Reference Read : {reference_read}')
+print(f'Match?         : {read == reference_read}')
